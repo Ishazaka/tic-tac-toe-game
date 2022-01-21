5bd9be462dc481b5dcd1f8c4bb46533814b87433
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-22 00:56:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ const changeturn = () =>{
 }
 
 
+// function to check win
+const checkwin = ()=>{
+  let boxtext = document.getElementsByClassName("boxtext");
+ let wins = [
+  [0,1,2],
+  [4,3,5],
+   [6,7,8],
+  [0,3,6],
+   [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+  
+ ]
