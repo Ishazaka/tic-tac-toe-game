975c647f88ee54eda63dc1f19feddd277a43869f
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-22 00:56:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let gameover = new Audio("gameover.mp3")
 
 let turn = "X";
 let isgameover = false;
+
+// function to change turn
+
+const changeturn = () =>{
+ return turn ==="X" ? "O": "X"
+
+}
+
+
